b5d15c950c1beb557a5495f7fc990de6aa5081c2
jeremiasbots <victorrbravo@icloud.com>
2025-03-08 20:30:18 -0500

    Fixing the problem of sending multiple instructions that do the same thing - src/touch_sensor_and_large_motor.py

--- src/touch_sensor_and_large_motor.py
+++ src/touch_sensor_and_large_motor.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.motor import LargeMotor, OUTPUT_B
+from ev3dev2.sound import Sound
 
 large_motor = LargeMotor(OUTPUT_B)
 ts = TouchSensor()
+sd = Sound()
+sd.beep()
+large_motor.reset()
 
+large_motor.on(50)
 while True:
-    if not ts.is_pressed:
-        large_motor.on(50)
-    else:
+    if ts.is_pressed and large_motor.is_running:
         large_motor.off()
+    if not ts.is_pressed and not large_motor.is_running:
+        large_motor.on(50)
